f16b0ac6e2595f0fc2bcc689ecbc60d3d574e32d
Asraf Ali <mdashraf9135@gmail.com>
2025-09-20 12:18:04 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Laravel</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo enim eveniet atque et consequuntur quae explicabo excepturi officiis nesciunt incidunt ut, ea totam cum nostrum hic ratione, qui voluptatem corporis!</p>
+    <ul>
+        <li>Hello Un-order List</li>
+        <li>Hello Un-order List</li>
+        <li>Hello Un-order List</li>
+        <li>Hello Un-order List</li>
+        <li>Hello Un-order List</li>
+    </ul>
+    <ol>
+        <li>Hello Order List</li>
+        <li>Hello Order List</li>
+        <li>Hello Order List</li>
+        <li>Hello Order List</li>
+        <li>Hello Order List</li>
+    </ol>
+</body>
+</html>
